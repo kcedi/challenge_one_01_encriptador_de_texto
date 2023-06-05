e4f4b37a9fea4d97e5a8d5bdc46c19dda56af684
Karen Cedillo <karen.kcedillo@gmail.com>
2023-06-05 12:38:16 -0600

    add function encryptText

--- scripts.js
+++ scripts.js
@@ -2,3 +2,29 @@ function autoGrow(element) {
     element.style.height = "5px";
     element.style.height = (element.scrollHeight)+"px";
 }
+
+function encryptText() {
+
+    // Obtenemos el texto del elemento textarea
+    let text = document.querySelector('#inputText').value;
+    let salidaDeTexto = document.querySelector("#outputText");
+
+    const encrypt = {
+        "e": "enter",
+        "i": "imes",
+        "a": "ai",
+        "o": "ober",
+        "u": "ufat"
+    };
+
+    text = text.replace(/[eiaou]/g, match => encrypt[match]);
+ 
+    // Escribimos el texto
+    salidaDeTexto.innerHTML = text;
+    salidaDeTexto.style.textAlign = "start";
+    salidaDeTexto.style.fontWeight = 400;
+    
+    // Ocultamos el texto del span
+    document.querySelector('#hideText').style.display = 'none';
+
+}
